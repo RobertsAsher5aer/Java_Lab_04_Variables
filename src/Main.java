c6736d66467d5e0032081c37c692071124c14207
RobertsAsher5ae <rober5ae@mai.uc.edu>
2024-05-29 18:14:00 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,87 @@
+@SuppressWarnings("ALL")
+public class Main {
+    public static void main(String[] args) {
+        // Initialize int variables
+        int intOperandA = 10;
+        int intOperandB = 3;
+        // Initialize sum, difference, product, quotient, and modulo to any value
+        int intSum = 13;
+        int intDifference = 7;
+        int intProduct = 30;
+        int intQuotient = 3;
+        int intModulo = 1;
+
+        // Initialize double variables
+        double doubleOperandA = 15.50;
+        double doubleOperandB = 5.30;
+        // Initialize sum, difference, product, and quotient to any value
+        double doubleSum = 20.80;
+        double doubleDifference = 10.20;
+        double doubleProduct = 82.15;
+        double doubleQuotient = 2.92;
+
+        // Create assignment statements using the variables for +,-,/,*, and %
+        intSum = intOperandA + intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        // Now create an output statement for each operation
+        System.out.println(" ");
+        System.out.println("Output statements for int operations: ");
+        System.out.println(" ");
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        System.out.println("The difference using ints of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        System.out.println("The product using ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        System.out.println("The quotient using ints of " + intOperandA + " and " + intOperandB + " is " + intQuotient);
+        System.out.println("The modulo using ints of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        // Do the same task for the double variables
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        //Output statements
+        System.out.println(" ");
+        System.out.println("Output statements for double operations: ");
+        System.out.println(" ");
+        System.out.println("The sum using doubles of " + doubleOperandA + " and " + doubleOperandB + " is "  + doubleSum);
+        System.out.println("The difference using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The product using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The quotient using doubles of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient);
+
+        // Variable declaration, lab part 2
+        int numKidsInFamily = 5;
+        String isItRaining = "No";
+        double gallonOfGasPrice = 3.46;
+        int myFavNum = 9;
+        int myShoeSize = 12;
+        String myBirthMonth = "April";
+        String myFullName = "Asher Elijah Roberts";
+
+        // Outputs
+        System.out.println(" ");
+        System.out.println("The number of kids in my family is " + numKidsInFamily);
+        System.out.println("The answer to is it raining? is " + isItRaining);
+        System.out.println("The price for a gallon of gas is currently $" + gallonOfGasPrice);
+        System.out.println("My favorite number is " + myFavNum);
+        System.out.println("My shoe size is " + myShoeSize);
+        System.out.println("My birth month is " + myBirthMonth);
+        System.out.println("My full name is " + myFullName);
+        System.out.println(" ");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
